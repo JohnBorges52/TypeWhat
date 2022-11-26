ba975a664829c805d14e7d4c25b607e47d5792d2
john <jrborges52@gmail.com>
2022-11-26 15:55:31 -0800

    finish btn play animation

--- guesswhat-front/src/App.js
+++ guesswhat-front/src/App.js
@@ -6,6 +6,7 @@ import MainPage from "./components/MainPage";
 import TopNavBar from "./components/TopNavBar";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import HomePage from "./components/HomePage";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<HomePage />} />
 
       </Routes>
     </div >
---
+++ guesswhat-front/src/components/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import'../styles/homepage.scss'
+
+export default function HomePage() {
+  return (
+    <div className='homepage-container'>
+
+      <div className='play-btn'>PLAY</div>
+      <div className='ranking-btn'>RANKING</div>
+
+
+
+
+
+
+
+
+    </div>
+
+  )
+}
