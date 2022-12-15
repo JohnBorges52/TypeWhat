5128d5c41119c3dadf623c16579ce7c8c8cc8da5
john <jrborges52@gmail.com>
2022-12-14 20:52:57 -0800

    add responsiviness to font and top nav bar

--- guesswhat-front/src/App.js
+++ guesswhat-front/src/App.js
@@ -1,4 +1,3 @@
-import './styles/app.scss'
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 
@@ -8,20 +7,28 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import HomePage from "./components/HomePage";
 import GamePage from "./components/GamePage";
+import './styles/style.scss'
 
 
 function App() {
   return (
     <div className="App">
-      <TopNavBar />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/play" element={<GamePage />} />
 
-      </Routes>
+      <TopNavBar />
+      <div className='main-container'>
+
+
+
+
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/play" element={<GamePage />} />
+
+        </Routes>
+      </div>
     </div >
   );
 }
--- guesswhat-front/src/components/TopNavBar.jsx
+++ guesswhat-front/src/components/TopNavBar.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import'../styles/top-nav-bar.scss'
 
+import Hamburger from 'hamburger-react'
+import { useState } from 'react'
+
 export default function TopNavBar() {
+
+  const [isOpen, setOpen] = useState(false)
+
+
+
+
   return (
     <div className='top--nav--bar'>
       <ul>
         <li><a href="#">LOGO</a></li>
         <li><a href="#">Login</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><Hamburger toggled={isOpen} toggle={setOpen} /></li>
       </ul>
-
+      
 
     </div>
   )
