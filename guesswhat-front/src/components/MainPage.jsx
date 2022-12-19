f46e6349e56c4726cc2c7074858bbb436555f5b2
john <jrborges52@gmail.com>
2022-12-18 16:34:04 -0800

    trying to implement font-awesome

--- guesswhat-front/src/components/MainPage.jsx
+++ guesswhat-front/src/components/MainPage.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import'../styles/mainpage.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
+
 
 
 export default function MainPage() {
   return (
+
+
+
+
+
+
     <div className='mainpage--container'>
 
-    
+      <div className='title-container'> 
+
+      
+      <h1 className='title-mainpage'>Welcome to TypeWhat!</h1>
+      <FontAwesomeIcon icon="fa-solid fa-typewriter" />
+      <h2 className='subtitle-mainpage'>Where you play and test how fast you type.</h2>
+
+      </div>
+
       <a  className='btn-link' href="/login"> LOGIN </a>
       <a  className='btn-link' href="/register"> REGISTER </a>
 
