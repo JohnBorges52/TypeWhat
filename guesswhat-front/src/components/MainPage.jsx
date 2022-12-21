1e743a1711face573b9b3c6dc61bcdd91ac5f9bb
john <jrborges52@gmail.com>
2022-12-20 17:04:27 -0800

    refactor the first page

--- guesswhat-front/src/components/MainPage.jsx
+++ guesswhat-front/src/components/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import'../styles/mainpage.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFeather } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -17,17 +18,49 @@ export default function MainPage() {
 
     <div className='mainpage--container'>
 
-      <div className='title-container'> 
+      <div className='top-bar-container'>
 
-      
-      <h1 className='title-mainpage'>Welcome to TypeWhat!</h1>
-      <FontAwesomeIcon icon="fa-solid fa-typewriter" />
-      <h2 className='subtitle-mainpage'>Where you play and test how fast you type.</h2>
+        <div className='title-container'>     
+        
+          <FontAwesomeIcon icon={faFeather} size="4x" className='fa-feather' />
+          <div className='h1-title-container'>
+            <h1 className='title-mainpage'>Type</h1>
+            <h1 className='title-mainpage'>What</h1>
+          </div>
+        </div>
+          <div className='title-mainpage-topnav'>
+            <div className='title-mainpage-topnav-right'>
+              <li><a href="#">Play</a></li>  
+              <li><a href="#">Ranking</a></li>  
+              <li><a href="#">About</a></li>  
+              <li><a href="#">another</a></li>  
+
+            </div>
+          </div>
 
       </div>
 
-      <a  className='btn-link' href="/login"> LOGIN </a>
-      <a  className='btn-link' href="/register"> REGISTER </a>
+      <div className='content-container'>
+        <div className='content-container-left'>
+
+          <div className='content-container-left-text'>
+            <h4> Where</h4>
+            <h2 > Typing Fast</h2>
+            <h3 > is better </h3>
+            </div>
+          <div className='content-container-left-subtext'>
+            <h5 > Test yor habilities to see how many words you can type in one minute and climb your way to the top of players. You will need to test your typing habilities as well as your vision in order to see the words moving.</h5>
+          </div>
+          <div className='content-container-left-subtext'>
+            <a  className='a-link-login' href="/login"> LOGIN </a>
+            <a  className='a-link-login' href="/register"> REGISTER </a>
+          </div>
+        
+        </div>
+      </div>
+
+
+
 
 
       
