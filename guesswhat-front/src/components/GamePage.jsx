d9dd74b6382e3053d7d1c9428ee891658654f111
john <jrborges52@gmail.com>
2023-02-02 19:05:07 -0800

    add gamepage close btn

--- guesswhat-front/src/components/GamePage.jsx
+++ guesswhat-front/src/components/GamePage.jsx
@@ -1,274 +1,241 @@
 import React from 'react'
-import'../styles/gamepage.scss'
+import '../styles/gamepage.scss'
 import axios from 'axios'
 import { useState, useRef } from 'react'
-import { useEffect } from 'react';
-import $ from 'jquery';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import { color } from '@mui/system';
+import { useEffect } from 'react'
+import $ from 'jquery'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+import { color } from '@mui/system'
 
+export default function GamePage(props) {
+  const [currentWord, setCurrentWord] = useState('')
+  const [userWord, setUserWord] = useState('')
+  const [playing, setPlaying] = useState(false)
+  const [counter, setCounter] = useState(0)
 
+  const [currentState, setCurrentState] = useState('StartBtn')
 
+  const [mileSecond, setMileSecond] = useState(0)
+  const [second, setSecond] = useState(0)
+  const [minute, setMinute] = useState(0)
 
+  const [runningTimer, setRunningTimer] = useState(false)
 
-export default function GamePage() {
+  useEffect(() => {
+    animateDiv()
+  }, [])
 
-    const[currentWord, setCurrentWord] = useState("");
-    const[userWord, setUserWord] = useState("");
-    const [playing, setPlaying] = useState(false)
-    const [counter, setCounter] = useState(0)
+  const makeNewPosition = () => {
+    let wordHeight = $('.currentWord').height()
+    let wordWidth = $('.currentWord').width()
 
-    const [currentState, setCurrentState] = useState("StartBtn")
+    let boxWidth = $('.randomwords-container').width() - wordWidth
 
-    const [mileSecond, setMileSecond] = useState(0)
-    const [second, setSecond] = useState(0)
-    const [minute, setMinute] = useState(0)
+    let min = $('.randomwords-container').offset().top
 
-    const [runningTimer, setRunningTimer] = useState(false)
-    
+    let max = $('.bottom-limit').offset().top - (wordHeight + wordHeight / 2)
 
+    let maxHeigh = Math.floor(Math.random() * (max - min))
+    let maxWidth = Math.floor(Math.random() * boxWidth)
 
-    useEffect(()=>{
-      animateDiv()
-    },[])
-
-
-   const makeNewPosition = () => {
-
-    let wordHeight = $(".currentWord").height();
-    let wordWidth = $(".currentWord").width();
-      
-    let boxWidth = $(".randomwords-container").width() - wordWidth;
-     
-    let min = $(".randomwords-container").offset().top
-    
-    
-    let max = $(".bottom-limit").offset().top - (wordHeight +(wordHeight/2) );
-
-    let maxHeigh = Math.floor(Math.random() * (max-min));
-    let maxWidth = Math.floor(Math.random() * boxWidth);
-
-    return [maxHeigh,maxWidth]
-   }
-   
-   
-   function animateDiv(){
-    let newq = makeNewPosition();
-    let oldq = $('.currentWord').offset();
-    let speed = calcSpeed([oldq.top, oldq.left], newq);
-    $('.currentWord').animate({ top: newq[0], left: newq[1] }, speed, function(){
-      animateDiv();        
-    });
-    
-   };
-
-   function calcSpeed(prev, next) {
-    
-    var x = Math.abs(prev[1] - next[1]);
-    var y = Math.abs(prev[0] - next[0]);
-    
-    var greatest = x > y ? x : y;
-    
-    var speedModifier = 0.5;
-
-    var speed = Math.ceil(greatest/speedModifier);
-
-    return speed;
+    return [maxHeigh, maxWidth]
   }
-   
 
-
-    useEffect(()=> {
-
-      const userWordInput = document.getElementById('input-word');
-
-      if(currentWord !== "" && userWord !== "" && currentWord === userWord){
-        userWordInput.value = "";
-        console.log("ACERTOU");
-        setCounter(counter+1);
-        animateCounter();
-        inputRef.current.focus();
-        generateRandomWord();
-        
+  function animateDiv() {
+    let newq = makeNewPosition()
+    let oldq = $('.currentWord').offset()
+    let speed = calcSpeed([oldq.top, oldq.left], newq)
+    $('.currentWord').animate(
+      { top: newq[0], left: newq[1] },
+      speed,
+      function () {
+        animateDiv()
       }
-
-    },[userWord])
-
-
-    const inputRef = useRef(null)
-
-
-    const generateRandomWord = () => {
-
-    const url = "https://api.api-ninjas.com/v1/randomword"
-    axios.get(url, {
-      headers: { 'X-Api-Key': 'Z32yDdS8nvmr8jzg+ypYTQ==zfzLwMaoKnftCKOB'}
-    })
-    .then((res)=>{
-      setCurrentWord(res.data.word);
-      inputRef.current.focus();
-     
-    })
+    )
   }
 
-  
-    if(runningTimer) {
+  function calcSpeed(prev, next) {
+    var x = Math.abs(prev[1] - next[1])
+    var y = Math.abs(prev[0] - next[0])
 
-      setTimeout(()=>{
-        if(playing && mileSecond > 100){
-          setMileSecond(mileSecond-1)
-        }
-        if(playing && mileSecond === 100){
-          setMileSecond(1000)
-        }
-        
-      }, 0.9)
-      
-      setTimeout(()=>{
-        if(playing) {
-          setSecond(second-1)
-        }
-        if(playing && minute > 0 && second === 0){
-          setMinute(minute-1)
-          setSecond(59)
-          
-        }
-        if(playing && minute === 0 && second === 0 ){
-          setSecond(0)
-          setMileSecond(0)
-          setMinute(0)
-          setPlaying(false)
-          
-        }
-      },1000)
+    var greatest = x > y ? x : y
+
+    var speedModifier = 0.5
+
+    var speed = Math.ceil(greatest / speedModifier)
+
+    return speed
+  }
+
+  useEffect(() => {
+    const userWordInput = document.getElementById('input-word')
+
+    if (currentWord !== '' && userWord !== '' && currentWord === userWord) {
+      userWordInput.value = ''
+      console.log('ACERTOU')
+      setCounter(counter + 1)
+      animateCounter()
+      inputRef.current.focus()
+      generateRandomWord()
     }
+  }, [userWord])
 
-    const formatTime = (time) => {
-      return String(time).padStart(2, '0')
-    }
+  const inputRef = useRef(null)
 
-    const animateCounter = () => {
-      const counterElement = document.getElementById('counter-id');
-      counterElement.classList.add("counter-animation");
+  const generateRandomWord = () => {
+    const url = 'https://api.api-ninjas.com/v1/randomword'
+    axios
+      .get(url, {
+        headers: { 'X-Api-Key': 'Z32yDdS8nvmr8jzg+ypYTQ==zfzLwMaoKnftCKOB' }
+      })
+      .then(res => {
+        setCurrentWord(res.data.word)
+        inputRef.current.focus()
+      })
+  }
 
-      const pictureElement = document.getElementById('animated-pic-id');
-      pictureElement.classList.add("points-span-animated");
-      
-      setTimeout(()=>{
-      counterElement.classList.remove("counter-animation");
-      pictureElement.classList.remove("points-span-animated");
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (playing) {
+        setSecond(second - 1)
+        console.log(second)
+      }
+      if (playing && minute > 0 && second === 0) {
+        setMinute(minute - 1)
+        setSecond(59)
+      }
+      if (playing && minute === 0 && second === 0) {
+        setSecond(0)
+        setMileSecond(0)
+        setMinute(0)
+        setPlaying(false)
+      }
+    }, 1000)
 
-      },1000)
+    // return () => {
+    //   clearTimeout(timer)
+    // }
+  }, [second])
 
-    }
+  const formatTime = time => {
+    return String(time).padStart(2, '0')
+  }
 
-    const hideElement = () => {
-      const element = document.getElementById("futureHiddenElement")
-    }
+  const animateCounter = () => {
+    const counterElement = document.getElementById('counter-id')
+    counterElement.classList.add('counter-animation')
 
-    
+    const pictureElement = document.getElementById('animated-pic-id')
+    pictureElement.classList.add('points-span-animated')
 
-    const startGame = () =>{
-      const element = document.getElementById("future-transparent")
-      element.classList.add("transparency-100");
+    setTimeout(() => {
+      counterElement.classList.remove('counter-animation')
+      pictureElement.classList.remove('points-span-animated')
+    }, 1000)
+  }
 
-      setInterval(() =>{
-      setPlaying(true); 
-      setRunningTimer(true); 
-      setSecond(59); 
-      setMinute(1);
-      setCounter(0);
-      setCurrentState("Playing");
-      element.classList.remove("transparency-100"); 
-      },8000)
-    }
+  const hideElement = () => {
+    const element = document.getElementById('futureHiddenElement')
+  }
 
+  const startGame = () => {
+    const element = document.getElementById('future-transparent')
+    element.classList.add('transparency-100')
+
+    setInterval(() => {
+      setPlaying(true)
+      setRunningTimer(true)
+      setSecond(59)
+      setMinute(1)
+      setCounter(0)
+      setCurrentState('Playing')
+      element.classList.remove('transparency-100')
+    }, 8000)
+  }
 
   return (
-    <div className='gamepage-container'>
+    <div className="gamepage-container">
+      <div className="close-btn-container">
+        <span onClick={props.onClose}>X</span>
+      </div>
 
-    <div className='randomwords-container'>
-      
-      <div className='start-btn-container'>
-        {currentState === "StartBtn" ?
-          <button className='start-btn' onClick={()=>{generateRandomWord(); startGame(); setCurrentState("Loading")}}>
-            START
-          </button>
-          :
-          <></>
-        }
-      
-      {currentState === "Loading" ?
-          <div className='countdown'>
-          <div className='countdown-circle'>
-            <span className='countdown-span'>3</span>
-            <span className='countdown-span'>2</span>
-            <span className='countdown-span'>1</span>
-            <span className='countdown-span'>GO!</span>
+      <div className="randomwords-container">
+        <div className="start-btn-container">
+          {currentState === 'StartBtn' ? (
+            <button
+              className="start-btn"
+              onClick={() => {
+                generateRandomWord()
+                startGame()
+                setCurrentState('Loading')
+              }}
+            >
+              START
+            </button>
+          ) : (
+            <></>
+          )}
+
+          {currentState === 'Loading' ? (
+            <div className="countdown">
+              <div className="countdown-circle">
+                <span className="countdown-span">3</span>
+                <span className="countdown-span">2</span>
+                <span className="countdown-span">1</span>
+                <span className="countdown-span">GO!</span>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+
+        <div className="currentWord" id="future-transparent">
+          {currentWord}
+        </div>
+      </div>
+      <div className="bottom-limit"></div>
+
+      <div className="game-information">
+        <div className="timer-container">
+          <div className="clock-pic-span"></div>
+          <span className="timer">
+            {formatTime(minute)} : {formatTime(second)}
+          </span>
+        </div>
+        {/* <ProgressBar now={progress} variant={"success"} style={{width: "150px" }} /> */}
+
+        <div className="game-information-points">
+          <span className="points-span" id="animated-pic-id"></span>
+
+          <span className="counter" id="counter-id">
+            {counter}
+          </span>
+          <div className="option-btn-div">
+            <div className="option-btn-container">
+              <span className="jump-pic-span"></span>
+              <span className="option-btn-text"> JUMP</span>
+            </div>
+
+            <div className="option-btn-container stop-bg">
+              <span className="stop-pic-span"></span>
+              <span className="option-btn-text"> STOP</span>
+            </div>
           </div>
         </div>
-        :        
-        <></>
-      }
-
-
-
-
-
       </div>
-            
-        <div className='currentWord' id='future-transparent'>  {currentWord} 
-      
-     </div>
-      
-    </div>
-    <div className='bottom-limit'></div>
-    
-    <div className='game-information'>
-      <div className='timer-container'>
 
-      <div className='clock-pic-span'></div>
-      <span className='timer'>{formatTime(minute)} : {formatTime(second)}</span>
-      
-
-    </div>
-      {/* <ProgressBar now={progress} variant={"success"} style={{width: "150px" }} /> */}
-
-      <div className='game-information-points'>
-
-
-        <span className='points-span' id='animated-pic-id'></span>
-
-
-        <span className='counter' id='counter-id'> {counter}</span>
-        <div className='option-btn-div'>
-
-        <div className='option-btn-container'>
-          <span className='jump-pic-span'></span>
-          <span className='option-btn-text'> JUMP</span>
-          
-        </div>
-        
-        
-        <div className='option-btn-container stop-bg'>
-        <span className='stop-pic-span'></span>
-          <span className='option-btn-text'> STOP</span>
-        </div>
-
-        </div>
-
+      <div className="typewords-container">
+        <input
+          className="input-words"
+          id="input-word"
+          ref={inputRef}
+          type="text"
+          onChange={e => {
+            setUserWord(e.target.value)
+          }}
+        />
       </div>
-    </div>
-    
-    
-
-    <div className='typewords-container'> 
-
-      <input className='input-words' id="input-word" ref={inputRef} type="text" onChange={(e)=>{setUserWord(e.target.value)}}  />
-
-        
-    </div>
-
     </div>
   )
 }
--- guesswhat-front/src/components/MainPage.jsx
+++ guesswhat-front/src/components/MainPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import'../styles/mainpage.scss'
+import '../styles/mainpage.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFeather } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
@@ -9,154 +9,176 @@ import Login from './Login'
 import GamePage from './GamePage'
 
 export default function MainPage() {
-
   const [isOpen, setOpen] = useState(false)
   const [loginOpen, setLoginOpen] = useState(true)
+  const [isGame, setIsGame] = useState(false)
 
   const addLoginClass = () => {
-
-    let element = document.getElementById("blur-container");
-    element.classList.add("blured-container");
+    let element = document.getElementById('blur-container')
+    element.classList.add('blured-container')
     setLoginOpen(true)
-
-
   }
   const removeLoginClass = () => {
-
-    let element = document.getElementById("blur-container");
-    element.classList.remove("blured-container");
+    let element = document.getElementById('blur-container')
+    element.classList.remove('blured-container')
     setLoginOpen(false)
-
   }
 
   const addDisplayClass = () => {
-
-    let element = document.getElementById("login-wrapper-display");
-    element.classList.add("login-wrapper-display");
+    let element = document.getElementById('login-wrapper-display')
+    element.classList.add('login-wrapper-display')
     setLoginOpen(true)
-
-
   }
   const removeDisplayClass = () => {
-
-    let element = document.getElementById("login-wrapper-display");
-    element.classList.remove("login-wrapper-display");
+    let element = document.getElementById('login-wrapper-display')
+    element.classList.remove('login-wrapper-display')
     setLoginOpen(false)
-
   }
-
-
-
-
-
-
 
   useEffect(() => {
     setLoginOpen(false)
-  },[])
-
-
+  }, [])
 
   return (
+    <>
+      <div className="bg-home-container" id="blur-container">
+        <div className="mainpage--container">
+          <div className="top-bar-container">
+            <div className="title-container">
+              <FontAwesomeIcon icon={faFeather} className="fa-feather" />
+              <div className="h1-title-container">
+                <h1 className="title-mainpage">Type</h1>
+                <h1 className="title-mainpage">What</h1>
+              </div>
+            </div>
+            <div className="title-mainpage-topnav">
+              <div className="title-mainpage-topnav-right">
+                <li className="hamburguer">
+                  <Hamburger
+                    toggled={isOpen}
+                    color="#c1fdfa"
+                    toggle={setOpen}
+                  />
+                </li>
 
-  <>
-  
-    <div className='bg-home-container' id='blur-container'>
+                {isOpen && (
+                  <>
+                    <li className="small-Li">
+                      <a href="/play">Play</a>
+                    </li>
+                    <li className="small-Li">
+                      <a href="#">Ranking</a>
+                    </li>
+                    <li className="small-Li">
+                      <a href="#">About</a>
+                    </li>
+                    <li className="small-Li">
+                      <a href="#">another</a>
+                    </li>
+                  </>
+                )}
 
-
-    <div className='mainpage--container'>
-
-      <div className='top-bar-container'>
-
-        <div className='title-container'>     
-        
-          <FontAwesomeIcon icon={faFeather}  className='fa-feather' />
-          <div className='h1-title-container'>
-            <h1 className='title-mainpage'>Type</h1>
-            <h1 className='title-mainpage'>What</h1>
-          </div>
-        </div>
-          <div className='title-mainpage-topnav'>
-            <div className='title-mainpage-topnav-right'>
-              <li className='hamburguer'><Hamburger toggled={isOpen}  color="#c1fdfa" toggle={setOpen} /></li>
-             
-             {isOpen &&
-             <>
-              <li className='small-Li'><a href="/play">Play</a></li>  
-              <li className='small-Li'><a href="#">Ranking</a></li>  
-              <li className='small-Li'><a href="#">About</a></li>  
-              <li className='small-Li'><a href="#">another</a></li>     
-             </> 
-            }         
-              
-              <li className='normal-Li'><a href="/play">Play</a></li>  
-              <li className='normal-Li'><a href="#">Ranking</a></li>  
-              <li className='normal-Li'><a href="#">About</a></li>  
-              <li className='normal-Li'><a href="#">another</a></li>
-
+                <li className="normal-Li">
+                  <a href="/play">Play</a>
+                </li>
+                <li className="normal-Li">
+                  <a href="#">Ranking</a>
+                </li>
+                <li className="normal-Li">
+                  <a href="#">About</a>
+                </li>
+                <li className="normal-Li">
+                  <a href="#">another</a>
+                </li>
+              </div>
             </div>
           </div>
 
-      </div>
-
-      <div className='content-container'>
-
-        <div className='content-container-left'>
-
-          <div className='content-container-left-text'>
-            <h4> Where</h4>
-            <span > Typing Fast</span>
-            <h3 > is better </h3>
+          <div className="content-container">
+            <div className="content-container-left">
+              <div className="content-container-left-text">
+                <h4> Where</h4>
+                <span> Typing Fast</span>
+                <h3> is better </h3>
+              </div>
+              <div className="content-container-left-subtext">
+                <h5>
+                  Test yor habilities to see how many words you can type in one
+                  minute and climb your way to the top of players. You will need
+                  to test your typing habilities as well as your vision in order
+                  to see the words moving.
+                </h5>
+              </div>
+              <div className="content-container-left-subtext-links">
+                <span
+                  className="a-link-login btn-login"
+                  onClick={() => {
+                    addLoginClass()
+                    removeDisplayClass()
+                  }}
+                >
+                  Login now
+                </span>
+                <a className="a-link-login btn-register" href="/register">
+                  Register here
+                </a>
+                <span
+                  className="a-link-login btn-register"
+                  href="#"
+                  onClick={() => {
+                    addLoginClass()
+                    setIsGame(true)
+                  }}
+                >
+                  PLAY NOW
+                </span>
+              </div>
             </div>
-          <div className='content-container-left-subtext'>
-            <h5> Test yor habilities to see how many words you can type in one minute and climb your way to the top of players. You will need to test your typing habilities as well as your vision in order to see the words moving.</h5>
-          </div>
-          <div className='content-container-left-subtext-links'>
-            <a  className='a-link-login btn-login' onClick={()=>{addLoginClass(); removeDisplayClass()}} > Login now</a>
-            <a  className='a-link-login btn-register' href='/register'> Register here </a>
-            <a  className='a-link-login btn-register' href='#' onClick={()=> addLoginClass()}> PLAY NOW </a>
-          </div>
-        
-        </div>
-        
-        <svg height="0" width="0" class="svg-clip" style={{position:"absolute"}}>
-    <defs>
-         <clipPath id="clip" clipPathUnits="objectBoundingBox">
-           
-           <path d="M1,.21 Q1,0 .81,.09L.187,.4 Q0,.5 .187,.59L.81,.90 Q1,1 1,.79Z" />
-        </clipPath>
-    </defs>
-</svg>
-        
-        <div className='content-container-right'>
-          <div className='triangle'>
-          </div>
-          <div className='triangle-behind'> </div>
 
-        </div>
+            <svg
+              height="0"
+              width="0"
+              class="svg-clip"
+              style={{ position: 'absolute' }}
+            >
+              <defs>
+                <clipPath id="clip" clipPathUnits="objectBoundingBox">
+                  <path d="M1,.21 Q1,0 .81,.09L.187,.4 Q0,.5 .187,.59L.81,.90 Q1,1 1,.79Z" />
+                </clipPath>
+              </defs>
+            </svg>
 
+            <div className="content-container-right">
+              <div className="triangle"></div>
+              <div className="triangle-behind"> </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-    </div>
-
-</div>
- {/* --------------LOGIN---------------------------- */}
- {/* {loginOpen && 
+      {/* --------------LOGIN---------------------------- */}
+      {/* {loginOpen && 
  <Login 
   onCloseLogin={()=> {
   removeLoginClass()
  }}
  />} */}
- 
- 
-<Login 
-  onCloseLogin={()=> {
-    addDisplayClass(); removeLoginClass();
-  }} />
 
-<GamePage />
-  
-  </>
+      <Login
+        onCloseLogin={() => {
+          addDisplayClass()
+          removeLoginClass()
+        }}
+      />
 
+      {isGame && (
+        <GamePage
+          onClose={() => {
+            setIsGame(false)
+            addDisplayClass()
+            removeLoginClass()
+          }}
+        />
+      )}
+    </>
   )
 }
