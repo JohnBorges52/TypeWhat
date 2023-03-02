fa50e76380e4df45818e4f69303f8f8a01ed197b
john <jrborges52@gmail.com>
2023-03-01 16:28:29 -0800

    add focus function when user clicks on Start

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/play" element={<GamePage />} />
+
 
 
         </Routes>
